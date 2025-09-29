a447d5826932db0c5edbaa7830537122bc7b3526
lionJ07 <lion22.j07@gmail.com>
2025-09-28 21:44:42 -0500

    Add associations in UserEntity
    - Added @OneToMany relationships to Reserva and CarritoDeCompra.
    - Annotated associations with @PodamExclude to avoid issues during tests.

--- src/main/java/co/edu/udistrital/mdp/back/entities/UserEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/UserEntity.java
@@ -1,10 +1,18 @@
 package co.edu.udistrital.mdp.back.entities;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
+import java.util.List;
 import lombok.Data;
     @Data
     @Entity
 public class UserEntity extends PersonEntity{
     private Integer loyaltypoints;
-    //ReservaEntity
+    @PodamExclude
+    @OneToMany(mappedBy = "usuario")
+    private List<Reserva> reservas;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "usuario")
+    private List<ShoppingCartEntity> carritos;
 }
