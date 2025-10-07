bd6808348ded57bab3cf12b0c606d6e1dd5bfb09
Felkinsio <anfelipcm@gmail.com>
2025-10-06 23:09:49 -0500

    fix: user must have the maping to the relation notifications

--- src/main/java/co/edu/udistrital/mdp/back/entities/UserEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/UserEntity.java
@@ -3,11 +3,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import uk.co.jemos.podam.common.PodamExclude;
+
 import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
     @Data
     @Entity
 public class UserEntity extends PersonEntity{
+    private String username;
     private Integer loyaltypoints;
     @PodamExclude
     @OneToMany(mappedBy = "user")
@@ -16,4 +22,8 @@ public class UserEntity extends PersonEntity{
     @PodamExclude
     @OneToOne(mappedBy = "user")
     private ShoppingCartEntity shoppingCart;
+
+    @PodamExclude
+    @ManyToMany
+    private List<NotificationEntity> notifications;
 }
