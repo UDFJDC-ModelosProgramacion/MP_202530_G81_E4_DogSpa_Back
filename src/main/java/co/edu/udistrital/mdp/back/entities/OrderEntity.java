fe5de19bd80e10ff2b5c1ab3fc053b17efc93b98
Felkinsio <anfelipcm@gmail.com>
2025-10-06 22:12:21 -0500

    fix: added onetomany relation with orderDetail

--- src/main/java/co/edu/udistrital/mdp/back/entities/OrderEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/OrderEntity.java
@@ -1,8 +1,9 @@
 package co.edu.udistrital.mdp.back.entities;
 import java.sql.Date;
-
+import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -14,6 +15,9 @@ public class OrderEntity extends BaseEntity {
     private Double totalAmount;
     private double discount;
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetailEntity> orderDetails;
+    
     @ManyToOne
     private UserEntity user;
 }
