e90a7f75810bb70ca765d7b12c360ade43912476
Felkinsio <anfelipcm@gmail.com>
2025-10-12 19:04:53 -0500

    Fix: Exceptions must extends RuntimeExceptions for test

--- src/main/java/co/edu/udistrital/mdp/back/exceptions/IllegalOperationException.java
+++ src/main/java/co/edu/udistrital/mdp/back/exceptions/IllegalOperationException.java
@@ -3,7 +3,7 @@ package co.edu.udistrital.mdp.back.exceptions;
 /*
  * Excepción que se lanza cuando se realiza una operación ilegal
  */
-public class IllegalOperationException extends Exception {
+public class IllegalOperationException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
