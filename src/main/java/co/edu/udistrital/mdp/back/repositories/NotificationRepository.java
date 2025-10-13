f58257b69b03c1906516cd83e0b56e9145b49c98
Felkinsio <anfelipcm@gmail.com>
2025-10-12 20:40:46 -0500

    fix: rename some methods to solve test failing

--- src/main/java/co/edu/udistrital/mdp/back/repositories/NotificationRepository.java
+++ src/main/java/co/edu/udistrital/mdp/back/repositories/NotificationRepository.java
@@ -8,9 +8,6 @@ import co.edu.udistrital.mdp.back.entities.NotificationEntity;
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long>{
     
-    
     List<NotificationEntity> findByUsers_Id(Long userId);
-    
-    
     List<NotificationEntity> findByUsers_IdAndReadFalse(Long userId);
 }
