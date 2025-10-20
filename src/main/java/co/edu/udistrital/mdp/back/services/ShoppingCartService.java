9417a49f30154d515bbc6a56d127584b3c74b649
Alexander <amoralesu@udistrital.edu.co>
2025-10-20 09:03:46 -0500

    refactor: update shopping cart service and entity

--- src/main/java/co/edu/udistrital/mdp/back/services/ShoppingCartService.java
+++ src/main/java/co/edu/udistrital/mdp/back/services/ShoppingCartService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import co.edu.udistrital.mdp.back.entities.ProductEntity;
 import co.edu.udistrital.mdp.back.entities.UserEntity;
 import co.edu.udistrital.mdp.back.entities.ShoppingCartEntity;
 import co.edu.udistrital.mdp.back.exceptions.EntityNotFoundException;
