aecaaa8c8c867b3a035981e312f7d205aa703b09
Felkinsio <anfelipcm@gmail.com>
2025-10-21 21:42:41 -0500

    feat: added new detailDtos

---
+++ src/main/java/co/edu/udistrital/mdp/back/dto/NotificationDetailDTO.java
@@ -0,0 +1,7 @@
+package co.edu.udistrital.mdp.back.dto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationDetailDTO extends NotificationDTO {
+    private List<UserDTO> users = new ArrayList<>();
+}
---
+++ src/main/java/co/edu/udistrital/mdp/back/dto/OrderDtlDTO.java
@@ -0,0 +1,7 @@
+package co.edu.udistrital.mdp.back.dto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDtlDTO extends OrderDTO {
+    private List<OrderDetailDTO> orderDetails = new ArrayList<>();
+}
