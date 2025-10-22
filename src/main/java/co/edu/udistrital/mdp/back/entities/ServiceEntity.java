e3759f91f740cdbf601dacce60647c166602bfb3
Felkinsio <anfelipcm@gmail.com>
2025-10-21 21:39:58 -0500

    just a space

--- src/main/java/co/edu/udistrital/mdp/back/entities/ServiceEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/ServiceEntity.java
@@ -1,7 +1,6 @@
 package co.edu.udistrital.mdp.back.entities;
 import java.util.ArrayList;
 import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
